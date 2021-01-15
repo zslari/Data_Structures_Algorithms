71c9fcf0b4bd20a569b9b4c5fc5dbf20e99f84b7
zslari <54786741+zslari@users.noreply.github.com>
2021-01-14 16:43:37 -0800

    Create Node.py

---
+++ Linked_Lists_and_Stacks/Node.py
@@ -0,0 +1,21 @@
+""" Node Class from ADT """
+
+
+class Node:
+    """ Node Class to create nodes of a linked list """
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self, newdata):
+        self.data = newdata
+
+    def setNext(self, newnext):
+        self.next = newnext
